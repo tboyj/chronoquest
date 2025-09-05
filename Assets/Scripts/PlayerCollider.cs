669995445989ed2d929943ebe59692bd7bda94e2
jacks <jackson.m.philips@gmail.com>
2025-09-04 22:24:50 -0400

    Added player movement

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Xml.Serialization;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerCollider : MonoBehaviour
 {
-    
+    public float speed;
+    public float shiftUp;
+
+    public Rigidbody rb;
+    public SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z);
+        rb.velocity = movement * speed;
+        if (Input.GetKey(KeyCode.LeftShift)) // Stamina & running
+        {
+            rb.velocity = movement * (speed + .5f);
+        }
+
+
+
+
+
+        if (x != 0)
+        {
+            if (x < 0)
+            {
+                sprite.flipX = true;
+            }
+            else
+            {
+                sprite.flipX = false;
+            }
+        }
     }
 }
