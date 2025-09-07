b55305b1990f0c5f082c5c9268ea6cf922565e54
jacks <jackson.m.philips@gmail.com>
2025-09-07 14:29:19 -0400

    Ui updates, added in game time, will add date soon

--- Assets/Scripts/DayAndNight.cs
+++ Assets/Scripts/DayAndNight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,21 +13,47 @@ public class DayAndNight : MonoBehaviour
     [Range(-360f, 0.005f)]
     public float timeInDay;
     private float rotationSpeed = .005f;
+    public TextMeshProUGUI timeText;
     void Start()
     {
-        
+        timeText.text = "00:00";
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-            float multiplier = lightCurve.Evaluate(timeInDay);
-            timeInDay += rotationSpeed * multiplier;
-        if (timeInDay <= -360f)
+        bool isPM = false;
+        string amOrPm = "AM";
+        float multiplier = lightCurve.Evaluate(timeInDay);
+        timeInDay += rotationSpeed * multiplier;
+        int hours = Mathf.FloorToInt((timeInDay + 360f) / 15f) + 6;
+        int minutes = Mathf.FloorToInt((((timeInDay + 360f) / 15f) + 6 - hours) * 60f);
+        if (hours % 13 == 0 && hours % 24 != 0)
+        {
+            isPM = true;
+        }
+        else if (hours % 24 == 0)
+        {
+            isPM = false;
+        }
+        hours %= 24;
+        string hNotMil = hours.ToString("00");
+        if (hours == 0)
+        {
+            hNotMil = "01";
+        }
+        if (isPM)
+        {
+            hNotMil = (hours - 12).ToString("00");
+            amOrPm = "PM";
+        }
+        else
+            amOrPm = "AM";
+        timeText.text =  hNotMil+ ":" + minutes.ToString("00")+" "+amOrPm;
+        if (timeInDay >= 0.005f)
         {
             DaysHandler.daysLived++;
-            timeInDay += 360f;
-
+            timeInDay = -360f;
         }
 
         sunBody.rotation = Quaternion.Euler(timeInDay, -90f, -90f);
--- Assets/Scripts/ItemStorable.cs
+++ Assets/Scripts/ItemStorable.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemCategory {Weapon, ProgressionItem, TimeStone, Tool, ReplenishItem, QuestItem, KeyItem, Miscellaneous };
+public enum ItemRarity {Common, Uncommon, Rare, Epic, Legendary, Exotic};
 [CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
+
 public class ItemStorable : ScriptableObject
 {
-    public GameObject obj;
-    public Texture texture;
+    public GameObject prefab;
+    public ItemCategory category;
+    
+    public Sprite sprite;
+    
     public string itemName;
+    public ItemRarity rarity;
+    public string description;
+    public bool canBeDropped;
+    public bool canBeGiven;
 }
