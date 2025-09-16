f4140688bd59e544a5cba7f9512c5bc962b21ea9
tboyj <jackson.m.philips@gmail.com>
2025-09-16 01:18:15 -0400

    Scaled back

---
+++ Assets/Scripts/TimedGateActivater.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class TimedGateActivater : ItemHandler
+{
+    public SpriteRenderer sprite;
+    public bool amITurnedOn = false;
+
+    [Range(0.5f, float.MaxValue)]
+    public float duration = 1f;
+
+    public Transform affectedObject;
+
+    void Start()
+    {
+        sprite = transform.Find("Object").GetComponent<SpriteRenderer>();
+        sprite.color = Color.green;
+    }
+
+    protected override void HandleInteraction()
+    {
+        if (playerInTrigger && Input.GetKeyDown(KeyCode.E) && !amITurnedOn)
+        {
+            StartCoroutine(OpenGateRoutine());
+        }
+    }
+
+    private IEnumerator OpenGateRoutine()
+    {
+        Debug.Log("Gate opening...");
+        sprite.color = Color.red;
+        amITurnedOn = true;
+
+        affectedObject.position += Vector3.up * 3;
+
+        yield return new WaitForSeconds(duration);
+
+        Debug.Log("Gate closing...");
+        sprite.color = Color.green;
+        amITurnedOn = false;
+
+        affectedObject.position += Vector3.down * 3;
+    }
+}
