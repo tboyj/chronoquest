68d6e426002e3b7b7b72574c33d995d72c7497c2
tboyj <jackson.m.philips@gmail.com>
2025-09-24 19:15:51 -0400

    repush

---
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public List<Quest> playerQuests = new List<Quest>();
+    public List<Quest> playerQuestsCompleted = new List<Quest>();
+    public Quest currentQuest;
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
