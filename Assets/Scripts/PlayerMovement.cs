3d450f49b1161a1cc01034aeffa87d8f625531e0
tboyj <jackson.m.philips@gmail.com>
2025-11-07 20:23:06 -0500

    jumping reduced since it didnt fit, fixed some barrier issues.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,51 +21,42 @@ private float verticalVelocity; // separate Y velocity tracking
         if (isGrounded)
         {
             if (verticalVelocity < 0)
-                verticalVelocity = -1f; // small downward push to keep grounded
+                verticalVelocity = -1f;
 
-            // Jump input
             if (Input.GetKeyDown(KeyCode.Space))
-            {
                 verticalVelocity = jumpForce;
-            }
         }
         else
         {
-            // Apply gravity while in air
-            verticalVelocity += gravityStrength * Time.deltaTime;
+            // Use fixedDeltaTime for consistency
+            verticalVelocity += gravityStrength * Time.fixedDeltaTime;
         }
 
-        if (Time.timeScale > 0)
-        {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-            Vector3 rawInput = new Vector3(x, 0, z);
-            rawInput = Vector3.ClampMagnitude(rawInput, 1f);
-            Vector3 input = transform.TransformDirection(rawInput);
+        Vector3 rawInput = new Vector3(x, 0, z);
+        rawInput = Vector3.ClampMagnitude(rawInput, 1f);
+        Vector3 input = transform.TransformDirection(rawInput);
 
-            float speed = moveSpeed;
-            if (Input.GetKey(KeyCode.LeftShift))
-                speed *= runMultiplier;
+        float speed = moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift)) speed *= runMultiplier;
 
-            Vector3 horizontalVelocity;
-            if (rawInput.magnitude > 0.01f)
-                horizontalVelocity = Vector3.Lerp(new Vector3(velocity.x, 0, velocity.z), input * speed, acceleration * Time.deltaTime);
-            else
-                horizontalVelocity = Vector3.Lerp(new Vector3(velocity.x, 0, velocity.z), Vector3.zero, deceleration * Time.deltaTime);
+        Vector3 horizontalVelocity;
+        if (rawInput.magnitude > 0.01f)
+            horizontalVelocity = Vector3.Lerp(new Vector3(velocity.x, 0, velocity.z), input * speed, acceleration * Time.deltaTime);
+        else
+            horizontalVelocity = Vector3.Lerp(new Vector3(velocity.x, 0, velocity.z), Vector3.zero, deceleration * Time.deltaTime);
 
-            // Combine horizontal and vertical movement
-            velocity = new Vector3(horizontalVelocity.x, verticalVelocity, horizontalVelocity.z);
-                if (controller.enabled)
-                {
-                    controller.Move(velocity * Time.deltaTime);
-                }
+        velocity = new Vector3(horizontalVelocity.x, verticalVelocity, horizontalVelocity.z);
 
-            // Flip character
-            if (x < 0) flip = true;
-            else if (x > 0) flip = false;
-        }
+        if (controller.enabled)
+            controller.Move(velocity * Time.deltaTime);
+
+        // Flip character
+        flip = x < 0 ? true : x > 0 ? false : flip;
     }
+
 
     /*
     private void HandleStairs(Vector3 move)
