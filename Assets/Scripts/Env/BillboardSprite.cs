dd1cc87165fea9538f7bbcd83b7e8070ebde2a27
tboyj <jackson.m.philips@gmail.com>
2025-12-11 13:04:48 -0500

    Added billboarding for spriterenderer, finished geometry for mirror reflection in dungeon

---
+++ Assets/Scripts/Env/BillboardSprite.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer sprite;
+    [Tooltip("E1: X axis, E2: Y axis, E3: Z axis")]
+    public bool[] lockAxis = new bool[3]; // X, Y, Z
+    private float spX;
+    private float spY;
+    private float spZ;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        spX = sprite.transform.rotation.eulerAngles.x;
+        spY = sprite.transform.rotation.eulerAngles.y;
+        spZ = sprite.transform.rotation.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main == null) return;
+
+        sprite.transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - sprite.transform.position);
+        if (lockAxis[0])
+        {
+            spY = sprite.transform.rotation.eulerAngles.y;
+            spZ = sprite.transform.rotation.eulerAngles.z;
+            sprite.transform.rotation = Quaternion.Euler(spX, sprite.transform.rotation.eulerAngles.y, sprite.transform.rotation.eulerAngles.z);
+        }
+        if (lockAxis[1])
+        {
+            spX = sprite.transform.rotation.eulerAngles.x;
+            spZ = sprite.transform.rotation.eulerAngles.z;
+            sprite.transform.rotation = Quaternion.Euler(sprite.transform.rotation.eulerAngles.x, spY, sprite.transform.rotation.eulerAngles.z);
+        }
+        if (lockAxis[2])
+        {
+            spX = sprite.transform.rotation.eulerAngles.x;
+            spY = sprite.transform.rotation.eulerAngles.y;
+            sprite.transform.rotation = Quaternion.Euler(sprite.transform.rotation.eulerAngles.x, sprite.transform.rotation.eulerAngles.y, spZ);
+        }
+    }
+}
